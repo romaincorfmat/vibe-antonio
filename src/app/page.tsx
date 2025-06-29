0ca37f17f0c7b94b964f9fd78db24afc581c6be9
romaincorfmat <romain.corfmat@gmail.com>
2025-06-29 18:03:53 +0800

    02 - database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div>
-      <Button variant="destructive">Click me</Button>
-    </div>
-  );
-}
+const HomePage = () => {
+  return <div>Hello World!</div>;
+};
+
+export default HomePage;
