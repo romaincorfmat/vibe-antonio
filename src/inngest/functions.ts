4190970c3688f42284c5839de0547dd18d355b39
romaincorfmat <romain.corfmat@gmail.com>
2025-06-30 16:32:55 +0800

    04 : background jobs

---
+++ src/inngest/functions.ts
@@ -0,0 +1,15 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "30s");
+
+    await step.sleep("wait-a-moment", "20s");
+
+    await step.sleep("wait-a-moment", "10s");
+
+    return { message: `Hello ${event.data.email}!` };
+  }
+);
